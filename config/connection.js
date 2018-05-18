dc07ef144fc93d11321fbe4bcf4217485b5a65a0
Matthew Paulino <matthewpaulino@Matthews-MacBook-Pro.local>
2018-05-17 19:04:36 -0700

    updated connection.js and removed notes that were too verbose

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,17 @@
 // Pull in required dependencies
 var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'root',
+  database: 'burger_db',
+  port: '8889'
+});
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'burger_db',
-    port: '8889'
-  });
-}
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('connected as id: ' + connection.threadId);
+});
 
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//   host: '127.0.0.1',
-//   user: 'root',
-//   password: 'root',
-//   database: 'burger_db',
-//   port: '8889'
-// });
+module.exports = connection;
 
-// connection.connect(function(err) {
-//   if (err) throw err;
-//   console.log('connected as id: ' + connection.threadId);
-// });
-
-// module.exports = connection;
